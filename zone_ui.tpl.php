951027c34d64db9da78d0eb45fe84c1cc4df6459
@himerus <jake.strawn@gmail.com>
2011-02-16 22:05:12 -0500

    making a first run at the zone configuration interface

---
+++ zone_ui.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template for rendering drag & drop zones for the Omega UI
+ * 
+ * Variables Available:
+ * - 
+ * - 
+ * - 
+ * - 
+ * - 
+ * - 
+ * - 
+ * - 
+ */
+?>
+<div class="container-12" <?php print $attributes; ?>>
+  <?php print render($regions); ?>
+</div>
